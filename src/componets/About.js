3d47f183cf0463aa8db8b610af1ea7eb89b723bc
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-05-29 16:57:10 -0700

    filled out app and index.js, started about.js

---
+++ src/componets/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <p className="content is-medium"> Full Stack Developer</p>
+      <hr />
+      {/* <img/> */}
+      <p className="content is-italic mt-4">
+       Hello
+      </p>
+      <p className="content">
+        I am a Full Stack Developer.
+      </p>
+    </div>
+  );
+}
+
+export default About;
