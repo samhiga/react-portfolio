31795b75a02d2ae5a0ba451e69b63a19fa2fe461
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-05-31 18:16:12 -0700

    added more styling to nav bar and about page

--- src/componets/About.js
+++ src/componets/About.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Profile from "../assets/img/Profile.jpeg";
+import "./About.css";
 
 function About() {
     return (
+      <div className="about-card">
       <div
         style={{
           display: 'flex',
@@ -24,8 +26,9 @@ function About() {
           experience in marketing. However, I am strongly interested in Web
           Development and Computer Science and am currently attending UC Berkeley's
           Full-Stack Bootcamp in hopes of becoming a Full-Stack or Back-End
-          Developer.
+          Developer. Please feel free to take a look at my Resume, Portfolio or reach out to me!
         </p>
+      </div>
       </div>
     );
   }
